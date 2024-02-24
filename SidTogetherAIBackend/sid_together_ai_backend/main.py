3fbeab56c8d1d4535e74f2f787444091eb77f917
Ethan Knox <ethan@staticdata.io>
2024-02-24 16:31:26 -0500

    centralized object schema in Sid

---
+++ SidTogetherAIBackend/sid_together_ai_backend/main.py
@@ -0,0 +1,23 @@
+
+# together uses a patched version of openai's client now
+from openai import OpenAI as Together
+
+
+from sid_together_ai_backend.enums import LLMModel
+
+class SidTogetherAIBackend:
+    """LLM adapter for Together AI"""
+    client: "Together"
+
+    def __init__(self,
+                 model: LLMModel,
+                 api_key: str):
+        """initiates the adapter with a model.
+        Explicitly create with api_key to avoid side effects and opaque behavior.
+        """
+        self.client = Together(
+            base_url = "https://api.together.xyz/v1",
+            api_key = api_key,
+            model = model.value
+            )
+
